d212b22a9a4cfb371e3943e643c0ba4d24cc0cbd
magdy0000 <magdy.mahmoud0000@outlook.com>
2022-04-08 15:51:32 +0200

    fix bugs number 2332 3232

--- app/src/main/java/com/magdy/gitwilldelete/MainActivity.kt
+++ app/src/main/java/com/magdy/gitwilldelete/MainActivity.kt
@@ -2,6 +2,7 @@ package com.magdy.gitwilldelete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
 
         ///dasdasdasddsadasd
         //dsddasdasd
+        Log.e("TAG", "onCreate: ", )
     }
 }
---
+++ app/src/main/java/com/magdy/gitwilldelete/Model.kt
@@ -0,0 +1,4 @@
+package com.magdy.gitwilldelete
+
+class Model {
+}
